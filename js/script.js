6f8d36a8b5ba7e7b5341156be56938849a975037
Gabriele-Musumeci <musumeci.gabriele@hotmail.com>
2020-09-17 16:10:32 +0200

    Prima stampa in corso

--- js/script.js
+++ js/script.js
@@ -1,18 +1,36 @@
 $(document).ready(function(){
-  alert("ciao");
 
+  // CALL TO SERVER
   $.ajax(
     {
       "url": "https://flynn.boolean.careers/exercises/api/array/music",
       "method": "GET",
       "success": function (data, stato) {
+
       console.log(data.response);
+      var returnToServer = data.response;
+      printAlbum(returnToServer);
+
       },
       "error": function (richiesta, stato, errori) {
       alert("E' avvenuto un errore. " + errore);
       }
     }
   );
+
+  function printAlbum(returnToServer){
+    var source = document.getElementById("album-template").innerHTML;
+    var template = Handlebars.compile(source);
+
+    for (var i = 0; i < returnToServer.length; i++) {
+      var album = returnToServer[i];
+      var html = template(album);
+
+      $(".album-list").append(html);
+    }
+  }
+
+
 
 
 
