551b56686a8f5ca7bcb0a493a7949ed11f469504
Gabriele-Musumeci <musumeci.gabriele@hotmail.com>
2021-03-18 16:02:57 +0100

    Finish

--- js/script.js
+++ js/script.js
@@ -6,53 +6,69 @@ $(document).ready(function(){
       "url": "https://flynn.boolean.careers/exercises/api/array/music",
       "method": "GET",
       "success": function (data, stato) {
-
-      console.log(data.response);
-      var returnToServer = data.response;
-      printAlbum(returnToServer);
+        console.log(data.response);
+        printAlbum(data.response);
 
       },
       "error": function (richiesta, stato, errori) {
-      alert("E' avvenuto un errore. " + errore);
+        alert("E' avvenuto un errore. " + errore);
       }
     }
   );
-
-  // FUNZIONE LINK HANDLEBARS
-  function printAlbum(returnToServer){
-
-    var source = $("#album-template").html();
-    var template = Handlebars.compile(source);
-
-    // CICLO DI STAMPA PER I SINGOLI DATI DI RITORNO DAL SERVER E INSERITI NEL TEMPLATE
-    for (var i = 0; i < returnToServer.length; i++) {
-      var album = returnToServer[i];
-      var html = template(album);
-
-      // STAMPA DATI
-      $(".album-list").append(html);
-    }
-  }
-
-  // FUNZIONE RICERCA GENERE tramite CHANGE che si attiva quando cambia valore
-  $(".music").change(
-    function(){
-      if ($(".cds-container").children().lenght > 0) {
-        var valueSelect = $(this).val();
-
-        if (valueSelect == "") {
-          $(".cd").show();
-        }else{
-          $(".cd").hide();
-          $(".cd[data-genere='"+ valueSelect +"']").show();
-        }
-      }
-    }
-  );
-
-
-
-
-
-
 });
+
+// FUNZIONE RICERCA GENERE tramite CHANGE che si attiva quando cambia valore
+$(".genere").change(function(){
+
+  if($(".cds-container").children().length > 0){
+    var valueSelect = $(this).val();
+
+    if(valueSelect == ""){
+      $(".cd").show();
+    } else{
+      $(".cd").hide();
+      $(".cd[data-genere = '"+valueSelect+"']").show();
+    }
+  } 
+});
+
+// FUNZIONE STAMPA ALBUM TRAMITE HANDLEBARS
+function printAlbum(returnToServer){
+
+  var source = $("#album-template").html();
+  var template = Handlebars.compile(source);
+
+  // CICLO DI STAMPA PER I SINGOLI DATI DI RITORNO DAL SERVER E INSERITI NEL TEMPLATE
+  for (var i = 0; i < returnToServer.length; i++) {
+
+    var album = returnToServer[i];      
+    var html = template(album);
+
+    // STAMPA DATI
+    $(".album-list").append(html);
+  }
+}
+
+
+
+
+// $(".genere").click(
+//     function() {
+//       var genreOption = $(this).val();
+//       if (genreOption != "All") {
+//         var cds = $(".cd").each(
+//           function() {
+//             var attribute = $(this).attr("data-genre");
+//             if (attribute != genreOption) {
+//               $(this).hide();
+//             } else {
+//               $(this).show();
+//             }
+//           }
+//         )
+//       } else {
+//         $(".cd").show();
+//       }
+    // });
+
+
